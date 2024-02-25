da9a823c23bef301655678d034a8433ebb767b39
Viktor <vmakksimov@gmail.com>
2024-02-25 22:03:56 +0200

    adding timeout to test

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 suite('About page', function() {
   test('Page title', async function() {
-
+    this.timeout(0);
     let res = await fetch("http://localhost:8888/about");
     let body = await res.text();
     assert.ok(body.includes("<title>About</title>"));
